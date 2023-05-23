b1b21d9ecfc6c5d27dc54c0b0954d9201efe2b07
PavanKumar190 <chinnapolupavankumar@gmail.com>
2023-05-23 06:04:33 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+        alt="img"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+        alt="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
